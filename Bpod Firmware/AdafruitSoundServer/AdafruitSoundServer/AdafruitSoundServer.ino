b92c7a8c4146e3a06be027ec171514dca510bc85
Akinori Mitani <amitani@ucsd.edu>
2015-11-10 17:07:30 -0800

    added AdafruitSoundServer and seperated it from SineWaveServer

--- Bpod Firmware/AdafruitSoundServer/AdafruitSoundServer/AdafruitSoundServer.ino
+++ Bpod Firmware/AdafruitSoundServer/AdafruitSoundServer/AdafruitSoundServer.ino
@@ -3,7 +3,6 @@
 
 This file is part of the Bpod Project
 Copyright (C) 2014 Joshua I. Sanders, Cold Spring Harbor Laboratory, NY, USA
-Copyright (C) 2015 Akinori Mitani, University of California, San Diego, CA, USA
 
 ----------------------------------------------------------------------------
 
@@ -18,49 +17,55 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include <SPI.h>
 #include <SD.h>
-#include <Adafruit_VS1053_SineWaveGenerator.h>
+#include <Adafruit_VS1053_FilePlayer.h>
 
-Adafruit_VS1053_SineWaveGenerator sineWaveGenerator = Adafruit_VS1053_SineWaveGenerator();
-
-volatile byte SoundIndex = 0;
-byte currentSoundIndex = 0;
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  // create breakout-example object!
+  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+  // create shield-example object!
+  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+  
 
 void setup()
 {
   Serial.begin(115200);
   
-  if (! sineWaveGenerator.begin()) { // initialise the music player
+  if (! musicPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
   }
   Serial.println(F("VS1053 found"));
   
-  sineWaveGenerator.volume[0]=100;
-  sineWaveGenerator.volume[1]=100;
-  sineWaveGenerator.init();
-  
-  Serial.println(F("Initialized"));
+  SD.begin(CARDCS);    // initialise the SD card
+    
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(20,20);
+
+  // Timer interrupts are not suggested, better to use DREQ interrupt!
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+
+  // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
 }
+
+byte serialByte=0;
 
 void loop()
 {
-  byte newSoundIndex = SoundIndex;
-  if(newSoundIndex==0) return;
-  if(newSoundIndex == currentSoundIndex) return;
-  
-  if(currentSoundIndex & 31) sineWaveGenerator.stopSineTest();
-  if(newSoundIndex & 31) sineWaveGenerator.startSineTest(newSoundIndex);
-  currentSoundIndex = newSoundIndex;
-}
-
-void serialEvent(){
   if (Serial.available()){
-    SoundIndex = Serial.read();
-    Serial.println(SoundIndex);
+    serialByte = Serial.read();
+    Serial.println(serialByte);
+    if(serialByte==255){
+      Serial.println("Stopping");
+      musicPlayer.stopPlaying();
+    }else if(serialByte>0){
+      String filename(String(serialByte)+".wav");
+      musicPlayer.startPlayingFile(filename.c_str());
+      Serial.println(filename);
+    }
   }
 }
-
-
-
